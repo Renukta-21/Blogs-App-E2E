daffd784b13195933fee4f5057c6a7b51bc67733
Daniel Martinez <edu211004@gmail.com>
2024-11-15 19:28:16 -0600

    writing tests

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,14 +1,27 @@
 import {test, expect} from '@playwright/test'
-import { describe } from 'node:test'
+import { beforeEach, describe } from 'node:test'
 
 describe('Blogs App', ()=>{
-  test('Frontend page can be opened', async({page})=>{
-    await page.goto(`http://localhost:5173/`)
-  
-    const locator = await page.getByText('Loin')
-    await expect(locator).toBeVisible()
-  
-  })
 
+  beforeEach(async()=>{
+    await page.goto(`http://localhost:5173/`)
+  })
+  
+  test('Frontend page can be opened', async({page})=>{
+    const locator = await page.getByText('Login')
+    await expect(locator).toBeVisible()
+  })
+  test('Login form can be opened', async({page})=>{
+    await page.getByRole('button', {name:'Login'}).click()
+    const locator =await page.getByText('Login to app')
+    await expect(locator).toBeVisible()
+
+    await page.getByRole('textbox').first().fill('Anibru')
+    await page.getByRole('textbox').last().fill('daniel211004')
+    await page.getByRole('button', {name:"Login"}).click()
+
+    const message =  await page.getByText('Error: Invalid username or password')
+    await expect(message).toBeVisible()
+  })
 
 })

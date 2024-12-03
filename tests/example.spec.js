739b5c9f855a74ec317713b6fcdf242acb02bf9d
Daniel Martinez <edu211004@gmail.com>
2024-12-03 11:56:23 -0600

    initial

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -5,19 +5,20 @@ describe('Blogs App', ()=>{
   beforeEach(async({page})=>{
     await page.goto(`http://localhost:5173/`)
   })
+  describe('login to app',()=>{
+    test('fails with incorrect credentials', async({page})=>{
+      const locator = await page.getByText('Login')
+      await expect(locator).toBeVisible()
   
-  test('Frontend page can be opened', async({page})=>{
-    const locator = await page.getByText('Login')
-    await expect(locator).toBeVisible()
-
-    await locator.click()
-    await expect(page.getByText('login to app')).toBeVisible()
-
-    await page.getByRole('textbox').first().fill('Hola')
-    await page.getByRole('textbox').last().fill('tonoto')
-    await page.getByRole('button', {name:'Login'}).click()
-    
-    await expect(page.getByText('Error')).toBeVisible()
+      await locator.click()
+      await expect(page.getByText('login to app')).toBeVisible()
+  
+      await page.getByRole('textbox').first().fill('Hola')
+      await page.getByRole('textbox').last().fill('tonoto')
+      await page.getByRole('button', {name:'Login'}).click()
+      
+      await expect(page.getByText('Error')).toBeVisible()
+    })
   })
 
 })
